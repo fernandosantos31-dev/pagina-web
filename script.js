f518a508334ae47c98d645dbc6c34ac184bdac9b
fernandosantos31-dev <fernando.santos31@escola.pr.gov.br>
2026-08-10 12:16:03 -0300

    10/08/2026

---
+++ script.js
@@ -0,0 +1,10 @@
+button{
+    padding;10px2 20px;
+    front-sile:18px;
+    cursor;pointer;
+    }
+const botao = document.getElementByld("tema");
+
+botao.addEventlistener("click",function() {
+    document.body.classlist.toggle("darck");
+    });
