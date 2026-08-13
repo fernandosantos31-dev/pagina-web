e5a3eed57e3b4e22a90fef70e98608074d2fce7e
fernandosantos31-dev <fernando.santos31@escola.pr.gov.br>
2026-08-13 12:05:48 -0300

    13/08/2026

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-button{
-    padding;10px2 20px;
-    front-sile:18px;
-    cursor;pointer;
-    }
-const botao = document.getElementByld("tema");
+
+const botao = document.getElementById("tema");
 
 botao.addEventlistener("click",function() {
     document.body.classlist.toggle("darck");
